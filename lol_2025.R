4e907d6ac5e53f57177ff76c83ac5e5980091d72
Ania <anna.kors.19.1@gmail.com>
2025-09-08 17:02:37 +0300

    Add filtering for easy heroes and grouping by herotype

--- lol_2025.R
+++ lol_2025.R
@@ -9,3 +9,9 @@ readLines("lol_2025.csv", n = 10)
 class(lol_2025)
 str(lol_2025)
 summary(lol_2025)
+
+# фільтр за найнижчою складністю та універсальністю
+lol_2025 %>% 
+  filter(difficulty == 1) %>%
+  filter(style <= 20) %>%
+  group_by (herotype)
